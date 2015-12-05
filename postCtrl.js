26c907f22cee602783bff3f207b47c60e0ea3371
Matthew Villwock <mtvillwock@gmail.com>
2015-12-04 17:20:03 -0800

    add all posts query

--- postCtrl.js
+++ postCtrl.js
@@ -2,8 +2,28 @@ angular.module('parseAuth')
     .controller("PostsCtrl", ['$scope',
         function($scope) {
 
+            $scope.getPosts = function() {
+                var Post = Parse.Object.extend("Post");
+                var query = new Parse.Query(Post);
+                query.find({
+                    success: function(results) {
+                        console.log("Successfully retrieved " + results.length + " objects.");
+                        // Do something with the returned Parse.Object values
+                        for (var i = 0; i < results.length; i++) {
+                            var object = results[i];
+                            console.log(object.id + ' - ' + object.get('title'));
+                        }
+                    },
+                    error: function(error) {
+                        alert("Error: " + error.code + " " + error.message);
+                    }
+                });
+            }
+            // fetch all posts on page load
+            $scope.getPosts();
+
             $scope.createPost = function(post) {
-              console.log("your post:", post);
+                console.log("your post:", post);
                 // define Post object in Parse DB
                 var Post = Parse.Object.extend("Post");
                 // instantiate Post object instance
@@ -16,7 +36,7 @@ angular.module('parseAuth')
 
                 newPost.save(null, {
                     success: function(post) {
-                      console.log("success post", post);
+                        console.log("success post", post);
                         // Execute any logic that should take place after the object is saved.
                         console.log('New object created with objectId: ' + post.id);
                     },
